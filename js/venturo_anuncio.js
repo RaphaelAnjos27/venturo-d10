88094032635ef878ca93098547788e78c23bc988
RaphaelAnjos27 <raphael.anjos@outlook.com.br>
2024-03-03 15:52:20 -0300

    configuranurando dominio

--- js/venturo_anuncio.js
+++ js/venturo_anuncio.js
@@ -5,7 +5,26 @@ export class VenturoAnuncio extends HTMLElement {
     connectedCallback() {
         // Inclua o conteúdo do componente no DOM quando ele for conectado
         this.innerHTML = `
-            <style>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3911900562799248"
+        crossorigin="anonymous"></script>
+        <!-- Anuncio Lateral -->
+        <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-3911900562799248"
+                data-ad-slot="2483162649"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+        <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+            
+            
+        `
+    }
+}
+
+/*
+<style>
                     .anuncio{
                         width: 100%;
                     }
@@ -19,7 +38,4 @@ export class VenturoAnuncio extends HTMLElement {
                 <a href="https://go.hotmart.com/Y86396770N" target="_blank">
                     <img class="anuncio" src="/static/imagens/anuncio/BannersHotmart_Página5.webp" alt="New York">
                 </a>
-            
-        `
-    }
-}
+*/
